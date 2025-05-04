2a1a63eb4e49c38329145e343680a1d77baf691c
IbtahajAkram <biryanizawwar@gmail.com>
2025-05-04 17:59:03 +0500

    add welcome subscribe email snd and done changes done

--- app/api/send-signup-email/route.js
+++ app/api/send-signup-email/route.js
@@ -1,31 +1,31 @@
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
 
-export default async function handler(req, res) {
-  if (req.method === "POST") {
-    const { userEmail } = req.body;
+// export default async function handler(req, res) {
+//   if (req.method === "POST") {
+//     const { userEmail } = req.body;
 
-    const transporter = nodemailer.createTransport({
-      service: "Gmail",
-      auth: {
-        user: "shopingonlines.pk@gmail.com",
-        pass: "xoxz zybb gvvh emaj", // Use App Password
-      },
-    });
+//     const transporter = nodemailer.createTransport({
+//       service: "Gmail",
+//       auth: {
+//         user: "shopingonlines.pk@gmail.com",
+//         pass: "xoxz zybb gvvh emaj", // Use App Password
+//       },
+//     });
 
-    try {
-      await transporter.sendMail({
-        from: `"ShopMart Website" <shopingonlines.pk@gmail.com>`,
-        to: "shopingonlines.pk@gmail.com",
-        subject: "New Google Signup",
-        text: `New user signed up with email: ${userEmail}`,
-      });
+//     try {
+//       await transporter.sendMail({
+//         from: `"ShopMart Website" <shopingonlines.pk@gmail.com>`,
+//         to: "shopingonlines.pk@gmail.com",
+//         subject: "New Google Signup",
+//         text: `New user signed up with email: ${userEmail}`,
+//       });
 
-      res.status(200).json({ success: true });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Email failed to send" });
-    }
-  } else {
-    res.status(405).json({ error: "Method not allowed" });
-  }
-}
+//       res.status(200).json({ success: true });
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ error: "Email failed to send" });
+//     }
+//   } else {
+//     res.status(405).json({ error: "Method not allowed" });
+//   }
+// }
